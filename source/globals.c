905a7a8ae3089a49bc4830cdb063b76c49db4628
Luc Rubio <luc@loociano.com>
2023-11-19 20:30:22 +0100

    Fix Copyright

--- source/globals.c
+++ source/globals.c
@@ -1,3 +1,11 @@
+/**
+ * VUEngine helloworld
+ *
+ * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
 #include <Game.h>
 #include <Debug.h>
 #include <StageEditor.h>
--- source/pluginsConfig.h
+++ source/pluginsConfig.h
@@ -1,3 +1,11 @@
+/**
+ * VUEngine helloworld
+ *
+ * © Jorge Eremiev <jorgech3@gmail.com> and Christian Radke <c.radke@posteo.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
 #ifndef PLUGINS_CONFIG_H_
 #define PLUGINS_CONFIG_H_
 
